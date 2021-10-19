263bf0ddff334e673ef5a09ce3a7c50f31634b42
Yulinarr <yulinar871@gmail.com>
2021-10-19 16:31:20 +0800

    menambahkan file

---
+++ Folder Baru/biodata diri Yulinar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	cout<< "               Biodata Diri"<<endl;
+	cout<< "............................................................."<<endl;
+	cout<< "Nama                    : Yulinar"<<endl;
+	cout<< "Nim                     : F1G121089"<<endl;
+	cout<< "Tempat Tanggal Lahir    : Mambo, 01 Desember 2003"<<endl;
+	cout<< "Alasan mengapa ingin masuk jurusan ilmu komputer adalah karena saya ingin lebi tahu tentang komputer atau laptop,"<<endl;
+	cout<< "tentang cara menggunakannya meupun cara-cara menggunakan program-programnya"<<endl;
+	
+	return 0;
+	
+}
